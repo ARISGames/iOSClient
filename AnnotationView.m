81f5843c487dc222d5bf6be93d383be12d100b62
Brian Deith <bcdeith@gmail.com>
2009-08-11 21:43:35 +0000

    MKMapview used for maps - Annotations still being worked on

---
+++ AnnotationView.m
@@ -0,0 +1,15 @@
+//
+//  AnnotationView.m
+//  ARIS
+//
+//  Created by Brian Deith on 8/11/09.
+//  Copyright 2009 Brian Deith. All rights reserved.
+//
+
+#import "AnnotationView.h"
+
+
+@implementation AnnotationView
+
+
+@end
