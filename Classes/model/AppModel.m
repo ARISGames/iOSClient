f777ef6d478e1d1d660e1cdf6d1e881c4e0df224
mrdavidgagnon <mrdavidgagnon@gmail.com>
2010-04-13 17:27:33 +0000

    storing lat/long data fixed

--- Classes/model/AppModel.m
+++ Classes/model/AppModel.m
@@ -476,8 +476,8 @@ static const int kEmptyValue = -1;
 						  newFileName,
 						  @"1",
 						  @"1",
-						  [NSString stringWithFormat:@"%d",playerLocation.coordinate.latitude],
-						  [NSString stringWithFormat:@"%d",playerLocation.coordinate.longitude],
+						  [NSString stringWithFormat:@"%f",playerLocation.coordinate.latitude],
+						  [NSString stringWithFormat:@"%f",playerLocation.coordinate.longitude],
 						  nil];
 	JSONConnection *jsonConnection = [[JSONConnection alloc]initWithArisJSONServer:self.jsonServerBaseURL 
 																	andServiceName:@"items" 
