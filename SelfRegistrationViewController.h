041fc8b1400be87013851e56ba56f8e53a6eb0c1
mrdavidgagnon <mrdavidgagnon@gmail.com>
2010-09-13 09:10:19 +0000

    minor UI improvements to login and self registration forms removed first name and last name from self registration, eliminating complex code

--- SelfRegistrationViewController.h
+++ SelfRegistrationViewController.h
@@ -8,39 +8,25 @@
 
 #import <UIKit/UIKit.h>
 #import "AppModel.h"
-#import "WaitingIndicatorView.h"
 
 
 
 @interface SelfRegistrationViewController : UIViewController <UITextFieldDelegate, UITextViewDelegate> {
 	AppModel *appModel;
-	WaitingIndicatorView *waitingIndicator;
-	IBOutlet UIScrollView *scrollView;
-	CGRect keyboardBounds; 
-	NSMutableArray *entryFields;
 	IBOutlet UITextField *userName;
 	IBOutlet UITextField *password;
-	IBOutlet UITextField *firstName;
-	IBOutlet UITextField *lastName;
 	IBOutlet UITextField *email;
 	IBOutlet UIButton *createAccountButton;
-	IBOutlet UILabel *messageLabel;
+
 	
 }
 
-@property (nonatomic, retain) WaitingIndicatorView *waitingIndicator;
-@property (nonatomic, retain) IBOutlet UIScrollView *scrollView;
-@property (nonatomic, retain) NSMutableArray *entryFields;
 @property (nonatomic, retain) IBOutlet UITextField *userName;
 @property (nonatomic, retain) IBOutlet UITextField *password;
-@property (nonatomic, retain) IBOutlet UITextField *firstName;
-@property (nonatomic, retain) IBOutlet UITextField *lastName;
 @property (nonatomic, retain) IBOutlet UITextField *email;
-@property (nonatomic, retain) IBOutlet UILabel *messageLabel;
 
 
 -(IBAction)submitButtonTouched: (id) sender;
--(void)submitRegistration;
 
 
 @end
--- SelfRegistrationViewController.m
+++ SelfRegistrationViewController.m
@@ -12,15 +12,10 @@
 
 @implementation SelfRegistrationViewController
 
-@synthesize waitingIndicator;
-@synthesize scrollView;
-@synthesize entryFields;
+
 @synthesize userName;
 @synthesize password;
-@synthesize firstName;
-@synthesize lastName;
 @synthesize email;
-@synthesize messageLabel;
 
 
 //Override init for passing title and icon to tab bar
@@ -29,11 +24,9 @@
     self = [super initWithNibName:nibName bundle:nibBundle];
     if (self) {
         self.title = NSLocalizedString(@"SelfRegistrationTitleKey", @"");
-		self.waitingIndicator = [[WaitingIndicatorView alloc] initWithWaitingMessage:@"Creating a New User" showProgressBar:NO];
 
 		appModel = [(ARISAppDelegate *)[[UIApplication sharedApplication] delegate] appModel];
 
-		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardNotification:)  name:UIKeyboardWillShowNotification object:nil];  
 		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(selfRegistrationFailure)  name:@"SelfRegistrationFailed" object:nil];  
 		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(selfRegistrationSuccess)  name:@"SelfRegistrationSucceeded" object:nil];  
     
@@ -44,45 +37,30 @@
 
 
 - (void)viewDidLoad {
-	messageLabel.text = NSLocalizedString(@"SelfRegistrationMessageKey",@"");
 	userName.placeholder = NSLocalizedString(@"UsernameKey",@"");
 	password.placeholder = NSLocalizedString(@"PasswordKey",@"");
 	email.placeholder = NSLocalizedString(@"EmailKey",@"");
-	firstName.placeholder = NSLocalizedString(@"FirstNameKey",@"");
-	lastName.placeholder = NSLocalizedString(@"LastNameKey",@"");
 	[createAccountButton setTitle:NSLocalizedString(@"CreateAccountKey",@"") forState:UIControlStateNormal];
+	
+	[userName becomeFirstResponder];
+
 	[super viewDidLoad];
 }
  
--(void)showLoadingIndicator{
-	[self.waitingIndicator show];
-}
-
--(void)removeLoadingIndicator{
-	[self.waitingIndicator dismiss];
-}
-
 
 - (IBAction)submitButtonTouched: (id) sender{
-	[self submitRegistration];
-}
+	[(ARISAppDelegate *)[[UIApplication sharedApplication] delegate] showNewWaitingIndicator:@"Creating a New User" displayProgressBar:NO];
 
-
--(void)submitRegistration {
-	[self showLoadingIndicator];
-	
-	//Check with the Server
-	//self.userName.text, self.password.text, self.firstName.text, self.lastName.text, self.email.text];
 	
 	[appModel registerNewUser:self.userName.text password:self.password.text 
-								   firstName:self.firstName.text lastName:self.lastName.text email:self.email.text]; 
+					firstName:@"" lastName:@"" email:self.email.text]; 
 }
 	
 
 -(void)selfRegistrationFailure{
 	NSLog(@"SelfRegistration: Unsuccessfull registration attempt, check network before giving an alert");
 
-	[self removeLoadingIndicator];
+	[(ARISAppDelegate *)[[UIApplication sharedApplication] delegate] removeNewWaitingIndicator];
 	
 	if (appModel.networkAlert) NSLog(@"SelfRegistration: Network is down, skip alert");
 	else{
@@ -90,91 +68,43 @@
 													   delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil];
 		[alert show];	
 		[alert release];
-	}		
+	}	
+	
+	[userName becomeFirstResponder];
+
 }
 
 -(void)selfRegistrationSuccess{
 	NSLog(@"SelfRegistration: New User Created Successfully");
 	
-	[self removeLoadingIndicator];
+	[(ARISAppDelegate *)[[UIApplication sharedApplication] delegate] removeNewWaitingIndicator];
 
-	UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Success" message:@"Your new User was Created. Go ahead and login."
+	UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Success" message:@"Your new User was Created. Please login."
 												   delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil];
 	[alert show];	
 	[alert release];
+
+	[userName resignFirstResponder];
+	[password resignFirstResponder];
+	[email resignFirstResponder];
+
+	
 	[self.navigationController popToRootViewControllerAnimated:YES];
 }
 	
 
-- (void)scrollViewToCenterOfScreen:(UIView *)theView {  
-    CGFloat viewCenterY = theView.center.y;  
-    CGRect applicationFrame = [[UIScreen mainScreen] applicationFrame];  
-	
-    CGFloat availableHeight = applicationFrame.size.height - keyboardBounds.size.height;    // Remove area covered by keyboard  
-	
-    CGFloat y = viewCenterY - availableHeight / 2.0;  
-    if (y < 0) {  
-        y = 0;  
-    }  
-    scrollView.contentSize = CGSizeMake(applicationFrame.size.width, applicationFrame.size.height + keyboardBounds.size.height);  
-    [scrollView setContentOffset:CGPointMake(0, y) animated:YES];  
-} 
-
-
-#pragma mark UITextFieldDelegate  
-
-- (void)textFieldDidBeginEditing:(UITextField *)textField {  
-    [self scrollViewToCenterOfScreen:textField];  
-}  
-
-#pragma mark UITextViewDelegate  
-
-- (void)textViewDidBeginEditing:(UITextView *)textView {  
-    [self scrollViewToCenterOfScreen:textView];  
-}  
-
-- (void)keyboardNotification:(NSNotification*)notification {  
-    NSDictionary *userInfo = [notification userInfo];  
-    NSValue *keyboardBoundsValue = [userInfo objectForKey:UIKeyboardBoundsUserInfoKey];  
-    [keyboardBoundsValue getValue:&keyboardBounds];  
-}  
-
-- (BOOL)textFieldShouldReturn:(UITextField *)textField {  
-    // Find the next entry field  
-    for (UIView *view in [self entryFields]) {  
-        if (view.tag == (textField.tag + 1)) {  
-            [view becomeFirstResponder];  
-            break;  
-        }  
-    }  
-	
-	if (textField.tag == 5) {
-		//Last field, go ahead and submit
-		[self submitRegistration];
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+	if (textField == userName) {
+		[password becomeFirstResponder];
+	}	
+	if(textField == password) {
+		[email becomeFirstResponder];
 	}
-	
-    return NO;  
-} 
-
-/* 
- Returns an array of all data entry fields in the view. 
- Fields are ordered by tag, and only fields with tag > 0 are included. 
- Returned fields are guaranteed to be a subclass of UIResponder. 
- */  
-- (NSMutableArray *)entryFields {  
-    if (!entryFields) {  
-        self.entryFields = [[NSMutableArray alloc] init];  
-        NSInteger tag = 1;  
-        UIView *aView;  
-        while (aView = [self.view viewWithTag:tag]) {  
-            if (aView && [[aView class] isSubclassOfClass:[UIResponder class]]) {  
-                [entryFields addObject:aView];  
-            }  
-            tag++;  
-        }  
-    }  
-    return entryFields;  
-}  
+	if(textField == email) {
+		[self submitButtonTouched:self];
+	}
+    return YES;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
@@ -183,8 +113,9 @@
 
 
 - (void)dealloc {
-	[waitingIndicator release];
-    [super dealloc];
+    
+	[[NSNotificationCenter defaultCenter] removeObserver:self];
+	[super dealloc];
 }
 
 
