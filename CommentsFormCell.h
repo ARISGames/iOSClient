62add5ed17358f7a7d9d401ff0328e2176b4a4ad
Brian Theil <brthiel@wisc.edu>
2011-06-08 04:58:08 +0000

    Fixed conflict

--- CommentsFormCell.h
+++ CommentsFormCell.h
@@ -9,20 +9,25 @@
 #import <UIKit/UIKit.h>
 #import "SCRRatingView.h"
 #import "Game.h"
-
+#import "commentsViewController.h"
 
 @interface CommentsFormCell : UITableViewCell {
     SCRRatingView *ratingView;
     UITextField *textField;
     UIButton *saveButton;
     Game *game;
+
+    commentsViewController *commentsCV;
+
     UIAlertView *alert;
+
 }
 
 @property (nonatomic, retain) UIAlertView *alert;
 @property(nonatomic,retain) IBOutlet SCRRatingView *ratingView;
 @property(nonatomic,retain) IBOutlet UITextField *textField;
 @property(nonatomic,retain) IBOutlet UIButton *saveButton;
+@property(nonatomic,retain) commentsViewController *commmentsCV;
 @property(nonatomic,retain) Game *game;
 
 - (IBAction)saveComment:(id)sender;
