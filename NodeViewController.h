5ef1235d3932ad22a3da44d42da4d34a4657631d
mrdavidgagnon <mrdavidgagnon@gmail.com>
2011-10-28 06:29:30 +0000

    Merged additional plaque changes from 1.5 branch

--- NodeViewController.h
+++ NodeViewController.h
@@ -16,18 +16,14 @@
 {
 	Node *node;
 	MPMoviePlayerViewController *mMoviePlayer; //only used if item is a video
-	CGSize imageSize;
 	UITableView *tableView;
 	UIButton *mediaPlaybackButton;
-    UIButton *contineuButton;
-	IBOutlet UIScrollView *scrollView;
     BOOL isLink;
     BOOL hasMedia;
     BOOL imageLoaded, webLoaded;
-    CGFloat newHeight;
-    CGFloat imageNewHeight;
+    
     AsyncImageView *mediaImageView;
-    UIActivityIndicatorView *spinner;
+    UIActivityIndicatorView *webViewSpinner;
     NSArray *cellArray;
 }
 
@@ -35,15 +31,9 @@
 @property(readwrite, assign) BOOL isLink;
 @property(readwrite, assign) BOOL hasMedia;
 @property(nonatomic, retain) IBOutlet UITableView *tableView;
-@property(nonatomic, retain) IBOutlet UIScrollView *scrollView;
-@property (nonatomic, assign) CGFloat  newHeight;
-@property (nonatomic, assign) CGFloat  imageNewHeight;
-@property(nonatomic, retain)UIActivityIndicatorView *spinner;
+@property(nonatomic, retain)UIActivityIndicatorView *webViewSpinner;
 @property(nonatomic,retain)AsyncImageView *mediaImageView;
-@property(nonatomic, retain) IBOutlet UIButton *continueButton;
 @property(nonatomic, retain)NSArray *cellArray;
--(void)imageFinishedLoading;
-- (void) refreshView;
-- (int) calculateTextHeight:(NSString *)text;
+
 @end
 
--- NodeViewController.m
+++ NodeViewController.m
@@ -15,6 +15,7 @@
 #import "AsyncImageView.h"
 #import "webpageViewController.h"
 #import "WebPage.h"
+#import <AVFoundation/AVFoundation.h>
 
 static NSString * const OPTION_CELL = @"option";
 
@@ -37,8 +38,7 @@ NSString *const kPlaqueDescriptionHtmlTemplate =
 
 
 @implementation NodeViewController
-@synthesize node, tableView, scrollView,isLink,newHeight, hasMedia,spinner, mediaImageView,imageNewHeight,cellArray;
-@synthesize continueButton;
+@synthesize node, tableView, isLink, hasMedia,webViewSpinner, mediaImageView, cellArray;
 
 // The designated initializer. Override to perform setup that is required before the view is loaded.
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
@@ -55,9 +55,9 @@ NSString *const kPlaqueDescriptionHtmlTemplate =
         self.isLink=NO;
         self.mediaImageView = [[AsyncImageView alloc]init];
         self.mediaImageView.delegate = self;
-        self.spinner = [[UIActivityIndicatorView alloc]initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
+        self.webViewSpinner = [[UIActivityIndicatorView alloc]initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
     }
-
+    
     return self;
 }
 
@@ -70,55 +70,45 @@ NSString *const kPlaqueDescriptionHtmlTemplate =
     self.title = self.node.name;
 	ARISAppDelegate *appDelegate = (ARISAppDelegate *)[[UIApplication sharedApplication] delegate];
     appDelegate.modalPresent = YES;
-    UIWebView *webView = [[UIWebView alloc]initWithFrame:CGRectMake(0, 0, 320, 60)];
-    webView.delegate = self;
     
-    NSString *htmlDescription = [NSString stringWithFormat:kPlaqueDescriptionHtmlTemplate, self.node.text];
-	[webView loadHTMLString:htmlDescription baseURL:nil];
-    webView.backgroundColor =[UIColor clearColor];
     
-    mediaImageView.contentMode = UIViewContentModeScaleAspectFit;
-    mediaImageView.frame = CGRectMake(0, 0, 320, 200);
-    self.imageNewHeight = mediaImageView.frame.size.height;
-
-    //Create Image/AV Cell
-    UITableViewCell *imageCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell1"];
-    
+    //Setup the Image View/Video Preview Image (if needed)
     Media *media = [[AppModel sharedAppModel] mediaForMediaId: self.node.mediaId];
-
+    
     //Check if the plaque has media
     if(([media.type isEqualToString: @"Video"] || [media.type isEqualToString: @"Audio"] || [media.type isEqualToString: @"Image"]) && media.url) hasMedia = YES;
     else hasMedia = NO;
     
-    if(hasMedia){
-        if(!mediaImageView.loaded)
-        [mediaImageView loadImageFromMedia:media];
-    }
+    //Create Image/AV Cell
+    UITableViewCell *imageCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"mediaCell"];
+    
     if ([media.type isEqualToString: @"Image"] && media.url) {
         NSLog(@"NodeVC: cellForRowAtIndexPath: This is an Image Plaque");
+        
+        self.mediaImageView.contentMode = UIViewContentModeScaleAspectFit;
+        
         if(!self.mediaImageView.loaded) {
             [self.mediaImageView loadImageFromMedia:media];
         }
         
+        //Setup the cell as an image
         imageCell.backgroundView = mediaImageView;
         imageCell.backgroundView.layer.masksToBounds = YES;
         imageCell.backgroundView.layer.cornerRadius = 10.0;
         imageCell.userInteractionEnabled = NO;
-    }
-    else if(([media.type isEqualToString: @"Video"] || [media.type isEqualToString: @"Audio"])&& media.url)
-    {
-        NSLog(@"NodeVC: cellForRowAtIndexPath: This is an A/V Plaque");
         
+        imageCell.frame = CGRectMake(0, 0, 320, 200);
+        self.mediaImageView.frame = CGRectMake(0, 0, 320, 200);
+
+    }
+    else if(([media.type isEqualToString: @"Video"] || [media.type isEqualToString: @"Audio"]) && media.url)
+    {
+        NSLog(@"NodeVC: This is an A/V Plaque");
         //Setup the Button
         mediaPlaybackButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 320, 240)];
         [mediaPlaybackButton addTarget:self action:@selector(playMovie:) forControlEvents:UIControlEventTouchUpInside];
-        [mediaPlaybackButton setBackgroundImage:[UIImage imageNamed:@"clickToPlay.png"] forState:UIControlStateNormal];
-        [mediaPlaybackButton setTitle:NSLocalizedString(@"PreparingToPlayKey",@"") forState:UIControlStateNormal];
         mediaPlaybackButton.enabled = NO;
-        mediaPlaybackButton.titleLabel.font = [UIFont boldSystemFontOfSize:24];
-        [mediaPlaybackButton setContentHorizontalAlignment:UIControlContentHorizontalAlignmentCenter];
-        [mediaPlaybackButton setContentVerticalAlignment:UIControlContentVerticalAlignmentBottom];
-        imageSize = mediaPlaybackButton.frame.size;
+        
         
         //Create movie player object
         mMoviePlayer = [[ARISMoviePlayerViewController alloc] initWithContentURL:[NSURL URLWithString:media.url]];
@@ -126,16 +116,20 @@ NSString *const kPlaqueDescriptionHtmlTemplate =
         mMoviePlayer.moviePlayer.shouldAutoplay = NO;
         [mMoviePlayer.moviePlayer prepareToPlay];
         
-        //Create thumbnail for button
-        UIImage *videoThumb = [[mMoviePlayer.moviePlayer thumbnailImageAtTime:(NSTimeInterval)1.0 timeOption:MPMovieTimeOptionNearestKeyFrame] retain];
-        //Resize thumb
+        //Syncronously Create a thumbnail for the button
+        UIImage *videoThumb = [[mMoviePlayer.moviePlayer thumbnailImageAtTime:(NSTimeInterval)1.0 timeOption:MPMovieTimeOptionExact] retain];
         
+        imageLoaded = YES;
+        NSLog(@"NodeVC: videoThumb frame size is : %f, %f", videoThumb.size.width, videoThumb.size.height);
         UIGraphicsBeginImageContext(CGSizeMake(320.0f, 240.0f));
         [videoThumb drawInRect:CGRectMake(0, 0, 320.0f, 240.0f)];
-        UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();    
+        UIImage *videoThumbSized = UIGraphicsGetImageFromCurrentImageContext();    
         UIGraphicsEndImageContext();
-        [mediaPlaybackButton setBackgroundImage:newImage forState:UIControlStateNormal];
+        [mediaPlaybackButton setBackgroundImage:videoThumbSized forState:UIControlStateNormal];
+        [videoThumb release];
+        [videoThumbSized release];
         
+        //Setup the cell as the video preview button
         imageCell.backgroundView = mediaPlaybackButton;
         imageCell.backgroundView.layer.masksToBounds = YES;
         imageCell.backgroundView.layer.cornerRadius = 10.0;
@@ -143,62 +137,67 @@ NSString *const kPlaqueDescriptionHtmlTemplate =
         imageCell.selectionStyle = UITableViewCellSelectionStyleNone;
         imageCell.frame = mediaPlaybackButton.frame;
     }
-
     
-    //Create Image/AV Cell
-    UITableViewCell *webCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell2"];
+    //Setup the Description Webview and begin loading content
+    UIWebView *webView = [[UIWebView alloc]initWithFrame:CGRectMake(0, 0, 300, 60)];
+    webView.delegate = self;
+    webView.backgroundColor =[UIColor clearColor];
+    NSString *htmlDescription = [NSString stringWithFormat:kPlaqueDescriptionHtmlTemplate, self.node.text];
+    webView.alpha = 0.0; //The webView will resore alpha once it's loaded to avoid the ugly white blob
+	[webView loadHTMLString:htmlDescription baseURL:nil];
     
+    //Create Description Web View Cell
+    UITableViewCell *webCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"descriptionCell"];
     webCell.userInteractionEnabled = NO;
     CGRect descriptionFrame = [webView frame];	
-    descriptionFrame.origin.x = 15;
-    descriptionFrame.origin.y = 15;
     [webView setFrame:descriptionFrame];
-    webCell.backgroundView =webView;
-    webCell.backgroundColor = [UIColor blackColor];
+    webCell.backgroundView = webView;
+    webCell.backgroundColor = [UIColor clearColor];
+    
+    webViewSpinner.center = webCell.center;
+    [webViewSpinner startAnimating];
+    webViewSpinner.backgroundColor = [UIColor clearColor];
+    [webCell addSubview:webViewSpinner];
 
-
-    //Create button cell
-    UITableViewCell *buttonCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell3"];
+    //Create continue button cell
+    UITableViewCell *buttonCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"continueButtonCell"];
     buttonCell.textLabel.text = @"Tap To Continue";
     buttonCell.textLabel.textAlignment = UITextAlignmentCenter;
+    
+    //Setup the cellArray
+    if(hasMedia) self.cellArray = [[NSArray alloc] initWithObjects:imageCell,webCell,buttonCell, nil];
+    else self.cellArray = [[NSArray alloc] initWithObjects:webCell,buttonCell, nil];
+    
+}
 
-    if(hasMedia)
-    self.cellArray = [[NSArray alloc] initWithObjects:imageCell,webCell,buttonCell, nil];
-    else
-        self.cellArray = [[NSArray alloc] initWithObjects:webCell,buttonCell, nil];
- 
-}
--(void)viewWillAppear:(BOOL)animated{
-    
-    
-}
--(void)viewDidDisappear:(BOOL)animated{
-   // self.isLink= NO;
-}
+#pragma mark UIWebViewDelegate Methods
+
 -(void)webViewDidFinishLoad:(UIWebView *)webView{
-    //if (!self.isLink){
-        float nHeight = [[webView stringByEvaluatingJavaScriptFromString:@"document.body.offsetHeight;"] floatValue];
-        self.newHeight = nHeight;
-        CGRect descriptionFrame = [webView frame];	
-        descriptionFrame.size = CGSizeMake(descriptionFrame.size.width,newHeight+5);
-        [webView setFrame:descriptionFrame];	
-
-        [tableView reloadData];
-    for(int x = 0; x < [webView.subviews count]; x ++){
-        if([[webView.subviews objectAtIndex:x] isKindOfClass:[UIActivityIndicatorView class]])
-            [[webView.subviews objectAtIndex:x] removeFromSuperview];
-    }
-   // }
+    
+    webView.alpha = 1.00;
+    
+    //Calculate the height of the web content
+    float newHeight = [[webView stringByEvaluatingJavaScriptFromString:@"document.body.offsetHeight;"] floatValue];
+    CGRect descriptionFrame = [webView frame];	
+    descriptionFrame.size = CGSizeMake(descriptionFrame.size.width,newHeight+5);
+    [webView setFrame:descriptionFrame];	
+    
+    //Find the webCell spinner and remove it
+    [webViewSpinner removeFromSuperview];
+    
+    //Find the description cell and update it's frame with the new size
     for(int x = 0; x < 2; x++){
-    if([[(UITableViewCell *)[self.cellArray objectAtIndex:x] reuseIdentifier] isEqualToString:@"Cell2"]){
-        [(UITableViewCell *)[self.cellArray objectAtIndex:x] setFrame:webView.frame];
+        if([[(UITableViewCell *)[self.cellArray objectAtIndex:x] reuseIdentifier] isEqualToString:@"descriptionCell"]){
+            [(UITableViewCell *)[self.cellArray objectAtIndex:x] setFrame:webView.frame];
+        }
     }
-    }
-    webLoaded = YES;
-    if((webLoaded && imageLoaded && hasMedia) ||(webLoaded && !hasMedia))    [self.tableView reloadData];
-
+    
+    [tableView reloadData];
+    
 }
--(BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType{
+-(BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request 
+navigationType:(UIWebViewNavigationType)navigationType{
+        
     if(self.isLink) {
         webpageViewController *webPageViewController = [[webpageViewController alloc] initWithNibName:@"webpageViewController" bundle: [NSBundle mainBundle]];
         WebPage *temp = [[WebPage alloc]init];
@@ -210,14 +209,26 @@ NSString *const kPlaqueDescriptionHtmlTemplate =
         
         return NO;
     }
-    else{
-           self.isLink = YES;
-        [spinner startAnimating];
-        spinner.center = webView.center;
-        spinner.backgroundColor = [UIColor blackColor];
-        [webView addSubview:spinner];
-        return YES;}
+    else return YES;
 }
+
+#pragma mark AsyncImageView Delegate Methods
+-(void)imageFinishedLoading{
+    NSLog(@"NodeVC: imageFinishedLoading with size: %f, %f",self.mediaImageView.frame.size.width,self.mediaImageView.frame.size.height);
+    
+    if(self.mediaImageView.image.size.width > 0){
+        [self.mediaImageView setContentScaleFactor:(float)(320/self.mediaImageView.image.size.width)];
+        self.mediaImageView.frame = CGRectMake(0, 0, 320, self.mediaImageView.contentScaleFactor*self.mediaImageView.image.size.height);
+        NSLog(@"NodeVC: Image resized to: %f, %f",self.mediaImageView.frame.size.width,self.mediaImageView.frame.size.height);
+        [(UITableViewCell *)[self.cellArray objectAtIndex:0] setFrame:mediaImageView.frame];
+    }
+    
+    [tableView reloadData];
+        
+}
+
+
+#pragma mark Button Handlers
 - (IBAction)backButtonTouchAction: (id) sender{
 	NSLog(@"NodeViewController: Notify server of Node view and Dismiss view");
 	
@@ -229,7 +240,7 @@ NSString *const kPlaqueDescriptionHtmlTemplate =
 	[self dismissModalViewControllerAnimated:NO];
     ARISAppDelegate *appDelegate = (ARISAppDelegate *)[[UIApplication sharedApplication] delegate];
     appDelegate.modalPresent=NO;
-
+    
 }
 
 - (IBAction)continueButtonTouchAction{
@@ -238,83 +249,39 @@ NSString *const kPlaqueDescriptionHtmlTemplate =
 	//Notify the server this item was displayed
 	[[AppServices sharedAppServices] updateServerNodeViewed:node.nodeId];
 	
-	
-	//[self.view removeFromSuperview];
+    //Remove thyself from the screen
 	[self dismissModalViewControllerAnimated:NO];
-    if((node.nodeId == [AppModel sharedAppModel].currentGame.completeNodeId) && ([AppModel sharedAppModel].currentGame.completeNodeId != 0)){
+    
+    //Check if this was the game complete Node and if so, display the "Start Over" tab
+    if((node.nodeId == [AppModel sharedAppModel].currentGame.completeNodeId) && 
+       ([AppModel sharedAppModel].currentGame.completeNodeId != 0)){
+        
         ARISAppDelegate* appDelegate = (ARISAppDelegate *)[[UIApplication sharedApplication] delegate];
         
         NSString *tab;
-        for(int i = 0;i < [appDelegate.tabBarController.customizableViewControllers count];i++)
-        {
+        for(int i = 0;i < [appDelegate.tabBarController.customizableViewControllers count];i++){
             tab = [[appDelegate.tabBarController.customizableViewControllers objectAtIndex:i] title];
             tab = [tab lowercaseString];
             
             if([tab isEqualToString:@"start over"])
-            {
                 appDelegate.tabBarController.selectedIndex = i;
-            }
-        }    }
-
+        }
+    }
+    
 }
 
 -(IBAction)playMovie:(id)sender {
     [mMoviePlayer.moviePlayer play];
 	[self presentMoviePlayerViewControllerAnimated:mMoviePlayer];
 }
--(void)imageFinishedLoading{
-    NSLog(@"NodeVC: imageFinishedLoading with size: %f, %f",self.mediaImageView.frame.size.width,self.mediaImageView.frame.size.height);
-    if(self.mediaImageView.image.size.width > 0){
-    [self.mediaImageView setContentScaleFactor:(float)(320/self.mediaImageView.image.size.width)];
-    self.mediaImageView.frame = CGRectMake(0, 0, 320, self.mediaImageView.contentScaleFactor*self.mediaImageView.image.size.height);
-        self.imageNewHeight = self.mediaImageView.frame.size.height;
-        NSLog(@"NEWSize: %f, %f",self.mediaImageView.frame.size.width,self.mediaImageView.frame.size.height);
-    }
-    imageLoaded = YES;
-    [(UITableViewCell *)[self.cellArray objectAtIndex:0] setFrame:mediaImageView.frame];
 
-    if((webLoaded && imageLoaded && hasMedia) ||(webLoaded && !hasMedia))   [self.tableView reloadData];
-
-}
-- (int) calculateTextHeight:(NSString *)text {
-	CGRect frame = CGRectMake(0, 0, self.view.bounds.size.width, 200000);
-	CGSize calcSize = [text sizeWithFont:[UIFont systemFontOfSize:18.0]
-					   constrainedToSize:frame.size lineBreakMode:UILineBreakModeWordWrap];
-	frame.size = calcSize;
-	frame.size.height += 0;
-	NSLog(@"Found height of %f", frame.size.height);
-	return frame.size.height;
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
-    // Release anything that's not essential, such as cached data
-}
-
-
-- (void)dealloc {
-	NSLog(@"NodeViewController: Dealloc");
-    
-	[mMoviePlayer release];
-	[node release];
-	[tableView release];
-	[scrollView release];
-	[mediaPlaybackButton release];
-    [continueButton release];
-	//[aWebView release];
-	//remove listeners
-	[[NSNotificationCenter defaultCenter] removeObserver:self];
-
-	
-    [super dealloc];
-}
 
 #pragma mark MPMoviePlayerController Notification Handlers
 
 
 - (void)movieLoadStateChanged:(NSNotification*) aNotification{
 	MPMovieLoadState state = [(MPMoviePlayerController *) aNotification.object loadState];
-
+    
 	if( state & MPMovieLoadStateUnknown ) {
 		NSLog(@"NodeViewController: Unknown Load State");
 	}
@@ -326,12 +293,12 @@ NSString *const kPlaqueDescriptionHtmlTemplate =
 	} 
 	if( state & MPMovieLoadStatePlaythroughOK ) {
 		NSLog(@"NodeViewController: Playthrough OK Load State");
-
+        
 	} 
 	if( state & MPMovieLoadStateStalled ) {
 		NSLog(@"NodeViewController: Stalled Load State");
 	} 
-		
+    
 }
 
 
@@ -354,9 +321,9 @@ NSString *const kPlaqueDescriptionHtmlTemplate =
 
 // Customize the appearance of table view cells.
 - (UITableViewCell *)tableView:(UITableView *)nibTableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {	
-
+    
     return [self.cellArray objectAtIndex:indexPath.section];
-
+    
 }
 
 // Customize the height of each row
@@ -371,8 +338,24 @@ NSString *const kPlaqueDescriptionHtmlTemplate =
 }
 
 
+#pragma mark Memory Management
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
+    // Release anything that's not essential, such as cached data
+}
 
 
-
+- (void)dealloc {
+	NSLog(@"NodeViewController: Dealloc");
+    
+	[mMoviePlayer release];
+	[node release];
+	[tableView release];
+	[mediaPlaybackButton release];
+	//remove listeners
+	[[NSNotificationCenter defaultCenter] removeObserver:self];
+    
+    [super dealloc];
+}
 
 @end
