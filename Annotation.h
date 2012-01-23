1ed37e124b895d35e30e97009a3db3da891d5fae
mrdavidgagnon <mrdavidgagnon@gmail.com>
2012-01-23 15:02:06 +0000

    matched atomic property in annotation to remove warnings

--- Annotation.h
+++ Annotation.h
@@ -22,8 +22,8 @@
 }
 
 @property (nonatomic, readonly) CLLocationCoordinate2D coordinate;
-@property (readwrite, copy) NSString *title;
-@property (readwrite, copy) NSString *subtitle;
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, copy) NSString *subtitle;
 @property(readwrite, assign) int iconMediaId;
 @property(readwrite, assign) nearbyObjectKind kind;
 @property (nonatomic, retain) Location *location;
