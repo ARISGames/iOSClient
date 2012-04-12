8508cdb53b6452e408a2c85e816d0b5cc9fdc1d8
mrdavidgagnon <mrdavidgagnon@gmail.com>
2012-04-12 21:16:45 +0000

    USe aspect fill for game details image

--- GameDetails.m
+++ GameDetails.m
@@ -203,7 +203,7 @@ NSString *const kGameDetailsHtmlTemplate =
         }
         else self.mediaImageView.image = [UIImage imageNamed:@"Default.png"];
         self.mediaImageView.frame = CGRectMake(0, 0, 320, 200);
-        self.mediaImageView.contentMode = UIViewContentModeScaleAspectFit;
+        self.mediaImageView.contentMode = UIViewContentModeScaleAspectFill;
 
         cell.backgroundView = mediaImageView;
         cell.backgroundView.layer.masksToBounds = YES;
