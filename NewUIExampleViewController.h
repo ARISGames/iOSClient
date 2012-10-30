208795a7c210231804ee25013be7ed91762b41d8
Jacob Hanshaw <hanshaw@wisc.edu>
2012-10-30 22:13:30 +0000

    Added example of new UIViewController

---
+++ NewUIExampleViewController.h
@@ -0,0 +1,16 @@
+//
+//  NewUIExampleViewController.h
+//  ARIS
+//
+//  Created by Jacob Hanshaw on 10/30/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+
+@interface NewUIExampleViewController : UIViewController {
+    
+}
+
+@end
