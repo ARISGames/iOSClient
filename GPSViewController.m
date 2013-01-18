ce89d4343a78ad421917295d2feeca08929a2a81
phildo <Phildo211@gmail.com>
2013-01-18 16:59:16 +0000

    fixed localization breaking map

--- GPSViewController.m
+++ GPSViewController.m
@@ -316,7 +316,7 @@ static float INITIAL_SPAN = 0.001;
             for (int i = 0; i < [[mapView annotations] count]; i++) {
                 BOOL match = NO;
                 NSObject <MKAnnotation>  *testAnnotation = [[mapView annotations] objectAtIndex:i];
-                if([testAnnotation respondsToSelector:@selector(title)] && ![testAnnotation.title isEqualToString:@"Current Location"]){
+                if([testAnnotation respondsToSelector:@selector(title)] && !(testAnnotation == mapView.userLocation)){
                     annotation = (Annotation *)testAnnotation;
                     if([[RootViewController sharedRootViewController].tabBarController.selectedViewController.title isEqualToString:@"Map"] &&[annotation.location respondsToSelector:@selector(hasBeenViewed)]) {
                         annotation.location.hasBeenViewed = YES;
