7423d059303d64ff8ea8426e17974af82c0fd811
David Gagnon <mrdavidgagnon@gmail.com>
2013-06-21 11:44:50 -0500

    Applied brand to reset password and create account

--- src/login/LoginViewController.mm
+++ src/login/LoginViewController.mm
@@ -165,14 +165,14 @@ using namespace std; //math.h undef's "isinf", which is used in mapkit...
 {
     [self resignKeyboard];
     ForgotPasswordViewController *forgotPassViewController = [[ForgotPasswordViewController alloc] init];
-    [[self navigationController] pushViewController:forgotPassViewController animated:NO];
+    [[self navigationController] pushViewController:forgotPassViewController animated:YES];
 }
 
 - (IBAction) newAccountButtonTouched:(id)sender
 {
     [self resignKeyboard];
     SelfRegistrationViewController *selfRegistrationViewController = [[SelfRegistrationViewController alloc] initWithDelegate:self];
-    [[self navigationController] pushViewController:selfRegistrationViewController animated:NO];
+    [[self navigationController] pushViewController:selfRegistrationViewController animated:YES];
 }
 
 - (void) registrationSucceededWithUsername:(NSString *)username password:(NSString *)password
