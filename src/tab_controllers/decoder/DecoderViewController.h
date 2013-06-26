e23d14b7e595b1c779762dee5c887327f2d4d140
Phildo <Phildo211@gmail.com>
2013-06-26 16:49:42 -0500

    fixed/cleaned a bunch of stuff in decoder tab (old unused buttons, events, etc...)

--- src/tab_controllers/decoder/DecoderViewController.h
+++ src/tab_controllers/decoder/DecoderViewController.h
@@ -18,21 +18,12 @@
 
 @interface DecoderViewController : ARISGamePlayTabBarViewController <UINavigationControllerDelegate, UIImagePickerControllerDelegate, ZXingDelegate>
 {
-	IBOutlet UIButton *qrScanButton;
-    IBOutlet UIButton *barcodeButton;
-    IBOutlet UIButton *imageScanButton;
 	IBOutlet UITextField *manualCode;
     NSString *resultText;
-    UIImagePickerController *imageMatchingImagePickerController;
     UIBarButtonItem *cancelButton;
 }
 
-@property (nonatomic) IBOutlet UIButton *qrScanButton;
-@property (nonatomic) IBOutlet UIButton *barcodeButton;
-
-@property (nonatomic) IBOutlet UIButton *imageScanButton;
 @property (nonatomic) IBOutlet UITextField *manualCode;
-@property (nonatomic) UIImagePickerController *imageMatchingImagePickerController;
 @property (nonatomic) NSString *resultText;
 @property (nonatomic) UIBarButtonItem *cancelButton;
 
