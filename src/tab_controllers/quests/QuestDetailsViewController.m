d090b719ad66611f187198880328ca3f2c153cdb
Phildo <Phildo211@gmail.com>
2013-08-19 13:18:45 -0500

    fixed sizing issues in iconquestvc

--- src/tab_controllers/quests/QuestDetailsViewController.m
+++ src/tab_controllers/quests/QuestDetailsViewController.m
@@ -20,7 +20,7 @@
     Quest *quest;
     ARISWebView *webView;
     
-    CGRect viewFrame;
+    BOOL hasAppeared;
     
     id<QuestDetailsViewControllerDelegate,StateControllerProtocol> __unsafe_unretained delegate;
 }
@@ -57,22 +57,28 @@ NSString *const kQuestDetailsHtmlTemplate =
 @synthesize webView;
 @synthesize webViewSpinner;
 
-- (id) initWithQuest:(Quest *)q delegate:(id<QuestDetailsViewControllerDelegate,StateControllerProtocol>)d frame:(CGRect)f;
+- (id) initWithQuest:(Quest *)q delegate:(id<QuestDetailsViewControllerDelegate,StateControllerProtocol>)d;
 {
     if(self = [super init])
     {
         self.quest = q;
-        viewFrame = f;
         delegate = d;
         self.hidesBottomBarWhenPushed = YES;
+        
+        hasAppeared = NO;
     }
     return self;
 }
 
-- (void) loadView
+- (void) viewWillAppear:(BOOL)animated
 {
-    [super loadView];
-    self.view.frame = viewFrame;
+    [super viewWillAppear:animated];
+    if(!hasAppeared) [self viewWillAppearFirstTime];
+}
+
+- (void) viewWillAppearFirstTime
+{
+    hasAppeared = YES;
 
     self.view.backgroundColor = [UIColor ARISColorOffWhite];
     self.title = self.quest.name;
