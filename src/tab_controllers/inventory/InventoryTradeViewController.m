76facbf7e9b00abce82351d342a9e7688cdc5219
David Gagnon <mrdavidgagnon@gmail.com>
2013-09-03 22:33:37 -0500

    Back button on trade is now a back arrow

--- src/tab_controllers/inventory/InventoryTradeViewController.m
+++ src/tab_controllers/inventory/InventoryTradeViewController.m
@@ -158,11 +158,12 @@
     self.itemsToTrade = [[NSMutableArray alloc] init];
     [self.tradeTableView reloadData];
     
-	self.navigationItem.leftBarButtonItem = 
-	[[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"BackButtonKey",@"")
-									 style:UIBarButtonItemStyleBordered
-									target:self 
-									action:@selector(backButtonTouchAction:)];	
+    
+    UIButton *backButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 27, 27)];
+    [backButton setImage:[UIImage imageNamed:@"arrowBack"] forState:UIControlStateNormal];
+    [backButton addTarget:self action:@selector(backButtonTouchAction:) forControlEvents:UIControlEventTouchUpInside];
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:backButton];
+
 }
 
 - (void) viewDidAppear:(BOOL)animated
