2c137ca3c5b319f5b790e92a999385950cea7fea
Emmanuel C <metalshreds@gmail.com>
2014-01-14 15:09:44 -0600

    Commented out move location code

--- integration/javascript/iphone/allTests.js
+++ integration/javascript/iphone/allTests.js
@@ -463,7 +463,7 @@ var exitToScripts = function() {
 var testDecoder = function() {
 
 
-test("Decoder: Access Normal Item", function(target, app){
+test("Scanner: Access Normal Item", function(target, app){
 	 
 	 //Go Into Decoder From MAP	
 	 app.navigationBar().buttons()["In-Game Menu"].tap();
@@ -495,7 +495,7 @@ test("Decoder: Access Normal Item", function(target, app){
 	 
 	 });
 	 
-test("Decoder: Requirements Not Met", function(target,app){
+test("Scanner: Requirements Not Met", function(target,app){
 		 
 		 
 	 //Go Into Decoder From MAP	
@@ -509,6 +509,7 @@ test("Decoder: Requirements Not Met", function(target,app){
 	 
 
 	 // Clear text Field
+	 target.delay(1);
 	 window.textFields()[0].tap();
 		 target.delay(1);
 	 target.frontMostApp().keyboard().keys()["Delete"].tap();
@@ -524,6 +525,7 @@ test("Decoder: Requirements Not Met", function(target,app){
 
 };
 
+
 							/*  ******* Create Image Asserter ******* */
 var imageAsserter = function(){
 		 
@@ -537,6 +539,89 @@ UIALogger.logMessage("Image Asserter Finished");
 		//target.delay(2);
 		//UIATarget.localTarget().captureAppScreenWithName('normalItem'); 
 */
+
+/////////////////// Test AREA
+
+var locationMigration = function() {
+ // speed is in meters/sec  
+ var points = [  
+                 {location:{latitude:40.828300000,longitude:-96.69941700}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.830158000,longitude:-96.700531000}, options:{speed:11, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.83086000,longitude:-96.700829000}, options:{speed:12, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.830860000,longitude:-96.700829000}, options:{speed:13, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.830551000,longitude:-96.70187400}, options:{speed:15, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.8304710004,longitude:-96.70233900}, options:{speed:15, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.830471000,longitude:-96.702339000}, options:{speed:9, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.830349000,longitude:-96.702332000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.830471000,longitude:-96.701782000}, options:{speed:3, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.830719000,longitude:-96.700958000}, options:{speed:6, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.8307190001,longitude:-96.700958000}, options:{speed:10, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.83005900,longitude:-96.700661000}, options:{speed:15, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.827221000,longitude:-96.699043000}, options:{speed:15, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}}, 
+                 {location:{latitude:40.825829000,longitude:-96.698181000}, options:{speed:12, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.82502000,longitude:-96.697578000}, options:{speed:10, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.822899000,longitude:-96.695374000}, options:{speed:5, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.821571000,longitude:-96.693329000}, options:{speed:5, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.821331000,longitude:-96.693047000}, options:{speed:6, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.820938000,longitude:-96.692703000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.820351000,longitude:-96.692413000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.819988000,longitude:-96.692307000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.819489000,longitude:-96.69229900}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.818859000,longitude:-96.692322000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.818501000,longitude:-96.692421000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.818008000,longitude:-96.692741000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.817131000,longitude:-96.693703000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.816898000,longitude:-96.693871000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.816490000,longitude:-96.694054000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.815819000,longitude:-96.694099000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.813580000,longitude:-96.694092000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.813580000,longitude:-96.694092000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.813560000,longitude:-96.685478000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.813499000,longitude:-96.685188000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.813450000,longitude:-96.682411000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},   
+                 {location:{latitude:40.813450000,longitude:-96.682411000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.804840000,longitude:-96.682419000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.799541000,longitude:-96.682487000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.797611000,longitude:-96.682487000}, options:{speed:15, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},   
+                 {location:{latitude:40.797508000,longitude:-96.682426000}, options:{speed:15, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.793510000,longitude:-96.682449000}, options:{speed:15, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.793190000,longitude:-96.682457000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.793121000,longitude:-96.682518000}, options:{speed:15, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude: 40.791660000,longitude:-96.682518000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude: 40.791580000,longitude:-96.682472000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude: 40.790779000,longitude:-96.682457000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.784431000,longitude:-96.682510000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.784431000,longitude:-96.682510000}, options:{speed:15, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.784451000,longitude:-96.687378000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.784451000,longitude:-96.687378000}, options:{speed:15, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.780891000,longitude:-96.687401000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.780891000,longitude:-96.687401000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}},  
+                 {location:{latitude:40.780880000,longitude:-96.687210000}, options:{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}} 
+                           ];  
+
+//set the app to watch for the country club
+
+var approachingLabelText;
+var approachingLogicWorked;
+
+//now drive
+for (var i = 0; i < points.length; i++){  
+        target.setLocationWithOptions(points[i].location,points[i].options);  
+     
+        approachingLabelText = window.staticTexts()[1].value();
+        
+        if(approachingLabelText == "You're approaching the Country Club!"){
+                approachingLogicWorked = true;
+                 UIALogger.logDebug("Approaching country club label was displayed");
+        }else
+                UIALogger.logDebug("Approaching country club label was not displayed");         
+}
+
+
+
+
+}
+
 
 							/*  ******* MAIN ******* */
 							
@@ -559,6 +644,9 @@ selectGame();
 					
 // Dismiss Initial Plaque
 initialPlaque();
+
+//Test Locations
+//locationMigration();
 
 //QuickTravel Test
 quickTravelTest();
@@ -584,9 +672,11 @@ exitToScripts();
 
 //Test Decoder
 testDecoder();
+
 							//////////////// End In Game Test and Reset 
 							
 // Reset Back to Login Screen
 resetToLoginScreen();
+
 
 
