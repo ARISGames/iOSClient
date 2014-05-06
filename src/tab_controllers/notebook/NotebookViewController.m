a3ce70d098362cc3bf5591140cd05ee8126f3712
Justin Moeller <jgmoeller@wisc.edu>
2014-05-06 13:09:13 -0500

    [FIXES #70472950] When the editor specifies notes are not allowed to be shared to the list or the map, the user cannot specify location or all notes

--- src/tab_controllers/notebook/NotebookViewController.m
+++ src/tab_controllers/notebook/NotebookViewController.m
@@ -79,8 +79,11 @@
     [self.view addSubview:fakeNavBG]; 
     [self.view addSubview:line1];
     [self.view addSubview:line2]; 
-    [self.view addSubview:line3]; 
-    [self.view addSubview:line4]; 
+    [self.view addSubview:line3];
+    if ([AppModel sharedAppModel].currentGame.allowShareNoteToList) {
+        [self.view addSubview:line4];
+    }
+    
     
     
     UIColor *fc = [UIColor whiteColor];
@@ -127,7 +130,9 @@
     labelSelectorButton.userInteractionEnabled = YES; 
     [labelSelectorButton addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(labelSelectorButtonTouched)]];
     
-    [self.view addSubview:allNotesButton];
+    if ([AppModel sharedAppModel].currentGame.allowShareNoteToList) {
+        [self.view addSubview:allNotesButton];
+    }
     [self.view addSubview:myNotesButton]; 
     [self.view addSubview:labelSelectorButton];     
     
@@ -148,9 +153,15 @@
     newImageButton.frame = CGRectMake(buttonPadding*5+buttonDiameter*2, 69, buttonDiameter, buttonDiameter); 
     newVideoButton.frame = CGRectMake(buttonPadding*7+buttonDiameter*3, 69, buttonDiameter, buttonDiameter); 
     
-    allNotesButton.frame      = CGRectMake(10, 134, self.view.frame.size.width-20, 30); 
-    myNotesButton.frame       = CGRectMake(10, 174, self.view.frame.size.width-20, 30); 
-    labelSelectorButton.frame = CGRectMake(10, 214, self.view.frame.size.width-20, 30);  
+    if ([AppModel sharedAppModel].currentGame.allowShareNoteToList) {
+        allNotesButton.frame      = CGRectMake(10, 134, self.view.frame.size.width-20, 30);
+        myNotesButton.frame       = CGRectMake(10, 174, self.view.frame.size.width-20, 30);
+        labelSelectorButton.frame = CGRectMake(10, 214, self.view.frame.size.width-20, 30);
+    }
+    else{
+        myNotesButton.frame       = CGRectMake(10, 134, self.view.frame.size.width-20, 30);
+        labelSelectorButton.frame = CGRectMake(10, 174, self.view.frame.size.width-20, 30);
+    }
     
     while(allNotesButton.subviews.count      > 0) [[allNotesButton.subviews      objectAtIndex:0] removeFromSuperview];
     while(myNotesButton.subviews.count       > 0) [[myNotesButton.subviews       objectAtIndex:0] removeFromSuperview]; 
