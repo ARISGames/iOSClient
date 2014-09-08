703eedcafc87dab1aa250de4aa347917b87f9ee5
David Gagnon <mrdavidgagnon@gmail.com>
2014-09-08 15:38:14 -0500

    Added a check to make sure a tab vc was successfully created before trying to add it to the game. Fixes a crash on any gme with custom tabs.

--- src/GamePlayTabSelectorViewController.m
+++ src/GamePlayTabSelectorViewController.m
@@ -195,7 +195,8 @@
             if(vc) [viewControllersDict setObject:vc forKey:tab.keyString];
         }
         
-        [viewControllers addObject:viewControllersDict[tab.keyString]];
+        if (viewControllersDict[tab.keyString])[viewControllers addObject:viewControllersDict[tab.keyString]];
+        else NSLog(@"ERROR: Tab from server could not be created. KeyString %@ should exist but does not have a matching VC", tab.keyString);
     }
 
     if(self.view) [tableView reloadData];
