27bba78edb39c7954b5b7130ade344057db18f2e
Phildo <Phildo211@gmail.com>
2015-03-13 10:57:34 -0500

    auto formatting (no code changes)

--- src/tab_controllers/notebook/NoteCell.m
+++ src/tab_controllers/notebook/NoteCell.m
@@ -16,17 +16,17 @@
 
 @interface NoteCell() <ARISMediaViewDelegate>
 {
-    UILabel *label;
-    UILabel *date;
-    UILabel *owner;
-    UILabel *desc;
-    ARISMediaView *preview;
-    CGRect previewFrameFull;
-    CGRect previewFrameSmall;
+  UILabel *label;
+  UILabel *date;
+  UILabel *owner;
+  UILabel *desc;
+  ARISMediaView *preview;
+  CGRect previewFrameFull;
+  CGRect previewFrameSmall;
 
-    Note *note;
+  Note *note;
 
-    id<NoteCellDelegate> __unsafe_unretained delegate;
+  id<NoteCellDelegate> __unsafe_unretained delegate;
 }
 @end
 
@@ -36,105 +36,104 @@
 
 - (id) initWithDelegate:(id<NoteCellDelegate>)d
 {
-    if(self = [super initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"notecell"])
-    {
-        delegate = d;
+  if(self = [super initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"notecell"])
+  {
+    delegate = d;
 
-        label = [[UILabel alloc] initWithFrame:CGRectMake(65,15,self.frame.size.width-85,14)];
-        label.font = [ARISTemplate ARISCellSubtextFont];
-        label.textColor = [UIColor ARISColorDarkGray];
-        label.adjustsFontSizeToFitWidth = NO;
-        date = [[UILabel alloc] initWithFrame:CGRectMake(10,35,65,14)];
-        date.font = [ARISTemplate ARISCellSubtextFont];
-        date.textColor = [UIColor ARISColorDarkBlue];
-        date.adjustsFontSizeToFitWidth = NO;
-        owner = [[UILabel alloc] initWithFrame:CGRectMake(65,35,self.frame.size.width-85,14)];
-        owner.font = [ARISTemplate ARISCellSubtextFont];
-        owner.textColor = [UIColor ARISColorDarkGray];
-        owner.adjustsFontSizeToFitWidth = NO;
-        desc = [[UILabel alloc] initWithFrame:CGRectMake(10,54,self.frame.size.width-self.frame.size.height-20,14)];
-        desc.font = [ARISTemplate ARISCellSubtextFont];
-        desc.textColor = [UIColor ARISColorDarkGray];
-        desc.adjustsFontSizeToFitWidth = NO;
-        preview = [[ARISMediaView alloc] initWithDelegate:self];
-        preview.clipsToBounds = YES;
-        preview.userInteractionEnabled = NO;
+    label = [[UILabel alloc] initWithFrame:CGRectMake(65,15,self.frame.size.width-85,14)];
+    label.font = [ARISTemplate ARISCellSubtextFont];
+    label.textColor = [UIColor ARISColorDarkGray];
+    label.adjustsFontSizeToFitWidth = NO;
+    date = [[UILabel alloc] initWithFrame:CGRectMake(10,35,65,14)];
+    date.font = [ARISTemplate ARISCellSubtextFont];
+    date.textColor = [UIColor ARISColorDarkBlue];
+    date.adjustsFontSizeToFitWidth = NO;
+    owner = [[UILabel alloc] initWithFrame:CGRectMake(65,35,self.frame.size.width-85,14)];
+    owner.font = [ARISTemplate ARISCellSubtextFont];
+    owner.textColor = [UIColor ARISColorDarkGray];
+    owner.adjustsFontSizeToFitWidth = NO;
+    desc = [[UILabel alloc] initWithFrame:CGRectMake(10,54,self.frame.size.width-self.frame.size.height-20,14)];
+    desc.font = [ARISTemplate ARISCellSubtextFont];
+    desc.textColor = [UIColor ARISColorDarkGray];
+    desc.adjustsFontSizeToFitWidth = NO;
+    preview = [[ARISMediaView alloc] initWithDelegate:self];
+    preview.clipsToBounds = YES;
+    preview.userInteractionEnabled = NO;
 
-        [self addSubview:label];
-        [self addSubview:date];
-        [self addSubview:owner];
-        [self addSubview:desc];
-        [self addSubview:preview];
+    [self addSubview:label];
+    [self addSubview:date];
+    [self addSubview:owner];
+    [self addSubview:desc];
+    [self addSubview:preview];
 
-    }
-    return self;
+  }
+  return self;
 }
 
 - (void) setFrame:(CGRect)frame
 {
-    [super setFrame:frame];
+  [super setFrame:frame];
 
-    if(!label) return; //views not initted
+  if(!label) return; //views not initted
 
-    previewFrameFull = CGRectMake(self.frame.size.width-(self.frame.size.height-4), 4, self.frame.size.height-8, self.frame.size.height-8);
-    previewFrameSmall = CGRectMake(self.frame.size.width-(self.frame.size.height-24), 24, self.frame.size.height-48, self.frame.size.height-48);
-    label.frame = CGRectMake(15,15,self.frame.size.width-previewFrameFull.size.width-10,14);
-    date.frame = CGRectMake(10,35,65,14);
-    owner.frame = CGRectMake(65,35,self.frame.size.width-85,14);
-    desc.frame = CGRectMake(10,54,self.frame.size.width-self.frame.size.height-20,14);
+  previewFrameFull = CGRectMake(self.frame.size.width-(self.frame.size.height-4), 4, self.frame.size.height-8, self.frame.size.height-8);
+  previewFrameSmall = CGRectMake(self.frame.size.width-(self.frame.size.height-24), 24, self.frame.size.height-48, self.frame.size.height-48);
+  label.frame = CGRectMake(15,15,self.frame.size.width-previewFrameFull.size.width-10,14);
+  date.frame = CGRectMake(10,35,65,14);
+  owner.frame = CGRectMake(65,35,self.frame.size.width-85,14);
+  desc.frame = CGRectMake(10,54,self.frame.size.width-self.frame.size.height-20,14);
 }
 
 - (void) setSelected:(BOOL)selected animated:(BOOL)animated
 {
-    [super setSelected:selected animated:animated];
+  [super setSelected:selected animated:animated];
 }
 
 - (void) populateWithNote:(Note *)n
 {
-    note = n;
+  note = n;
 
-    label.text = @"";
-    //if(n.tags.count > 0) label.text = ((NoteTag *)[n.tags objectAtIndex:0]).text;
-    label.frame = CGRectMake(15,15,self.frame.size.width-previewFrameFull.size.width-10,14);
-    NSDateFormatter *format = [[NSDateFormatter alloc] init];
-    [format setDateFormat:@"MM/dd/yy"];
-    date.text = [format stringFromDate:n.created];
-    //owner.text = n.owner.display_name;
-    desc.text = n.desc;
+  label.text = @"";
 
-    if(n.media_id) [preview setMedia:[_MODEL_MEDIA_ mediaForId:n.media_id]];
-    else           [preview setMedia:nil];
-    [preview setFrame:previewFrameFull];
+  label.frame = CGRectMake(15,15,self.frame.size.width-previewFrameFull.size.width-10,14);
+  NSDateFormatter *format = [[NSDateFormatter alloc] init];
+  [format setDateFormat:@"MM/dd/yy"];
+  date.text = [format stringFromDate:n.created];
+  desc.text = n.desc;
+
+  if(n.media_id) [preview setMedia:[_MODEL_MEDIA_ mediaForId:n.media_id]];
+  else           [preview setMedia:nil];
+  [preview setFrame:previewFrameFull];
 }
 
 - (void) setPreviewMedia:(Media *)m
 {
-    [preview setImage:nil];
-    if(!m) return;
+  [preview setImage:nil];
+  if(!m) return;
 
-    if([m.type isEqualToString:@"IMAGE"])
-    {
-        [preview setFrame:previewFrameFull];
-        [preview setDisplayMode:ARISMediaDisplayModeAspectFill];
-        [preview setMedia:m];
-    }
-    if([m.type isEqualToString:@"VIDEO"])
-    {
-        [preview setFrame:previewFrameSmall];
-        [preview setDisplayMode:ARISMediaDisplayModeAspectFit];
-        [preview setImage:[UIImage imageNamed:@"video.png"]];
-    }
-    if([m.type isEqualToString:@"AUDIO"])
-    {
-        [preview setFrame:previewFrameSmall];
-        [preview setDisplayMode:ARISMediaDisplayModeAspectFit];
-        [preview setImage:[UIImage imageNamed:@"microphone.png"]];
-    }
+  if([m.type isEqualToString:@"IMAGE"])
+  {
+    [preview setFrame:previewFrameFull];
+    [preview setDisplayMode:ARISMediaDisplayModeAspectFill];
+    [preview setMedia:m];
+  }
+  if([m.type isEqualToString:@"VIDEO"])
+  {
+    [preview setFrame:previewFrameSmall];
+    [preview setDisplayMode:ARISMediaDisplayModeAspectFit];
+    [preview setImage:[UIImage imageNamed:@"video.png"]];
+  }
+  if([m.type isEqualToString:@"AUDIO"])
+  {
+    [preview setFrame:previewFrameSmall];
+    [preview setDisplayMode:ARISMediaDisplayModeAspectFit];
+    [preview setImage:[UIImage imageNamed:@"microphone.png"]];
+  }
 }
 
 - (BOOL) ARISMediaViewShouldPlayButtonTouched:(ARISMediaView *)amv
 {
-    return NO;
+  return NO;
 }
 
 @end
--- src/tab_controllers/notebook/NotebookNotesViewController.m
+++ src/tab_controllers/notebook/NotebookNotesViewController.m
@@ -21,19 +21,19 @@ const long VIEW_MODE_TAG  = 2;
 
 @interface NotebookNotesViewController() <UITableViewDataSource, UITableViewDelegate, InstantiableViewControllerDelegate, NoteCellDelegate, NoteViewControllerDelegate, UISearchBarDelegate>
 {
-    UITableView *table;
-    UISearchBar *searchBar;
+  UITableView *table;
+  UISearchBar *searchBar;
 
-    UILabel *navTitleLabel;
-    UIView *navTitleView;
+  UILabel *navTitleLabel;
+  UIView *navTitleView;
 
-    NSArray *filteredNotes;
+  NSArray *filteredNotes;
 
-    long viewMode;
-    Tag *filterTag;
-    NSString *filterText;
+  long viewMode;
+  Tag *filterTag;
+  NSString *filterText;
 
-    id <NotebookNotesViewControllerDelegate> __unsafe_unretained delegate;
+  id <NotebookNotesViewControllerDelegate> __unsafe_unretained delegate;
 }
 
 @end
@@ -42,217 +42,217 @@ const long VIEW_MODE_TAG  = 2;
 
 - (id) initWithDelegate:(id<NotebookNotesViewControllerDelegate>)d
 {
-    if(self = [super init])
-    {
-        viewMode = VIEW_MODE_MINE;
-        filterText = @"";
+  if(self = [super init])
+  {
+    viewMode = VIEW_MODE_MINE;
+    filterText = @"";
 
-        delegate = d;
-        _ARIS_NOTIF_LISTEN_(@"MODEL_PLAYER_TRIGGERS_AVAILABLE",self,@selector(newNoteListAvailable),nil);
-    }
-    return self;
+    delegate = d;
+    _ARIS_NOTIF_LISTEN_(@"MODEL_PLAYER_TRIGGERS_AVAILABLE",self,@selector(newNoteListAvailable),nil);
+  }
+  return self;
 }
 
 - (void) loadView
 {
-    [super loadView];
-    self.view.backgroundColor = [UIColor whiteColor];
+  [super loadView];
+  self.view.backgroundColor = [UIColor whiteColor];
 
-    searchBar = [[UISearchBar alloc] init];
-    searchBar.delegate = self;
-    searchBar.showsCancelButton = NO;
+  searchBar = [[UISearchBar alloc] init];
+  searchBar.delegate = self;
+  searchBar.showsCancelButton = NO;
 
-    navTitleView = [[UIView alloc] init];
+  navTitleView = [[UIView alloc] init];
 
-    navTitleLabel = [[UILabel alloc] init];
-    navTitleLabel.font = [ARISTemplate ARISTitleFont];
-    navTitleLabel.textAlignment = NSTextAlignmentCenter;
-    switch(viewMode)
-    {
-        case VIEW_MODE_MINE: navTitleLabel.text = NSLocalizedString(@"NotebookMyNotesKey", @"");    break;
-        case VIEW_MODE_ALL:  navTitleLabel.text = NSLocalizedString(@"NotebookAllNotesKey", @"");   break;
-        case VIEW_MODE_TAG:  navTitleLabel.text = filterTag.tag; break;
-    }
+  navTitleLabel = [[UILabel alloc] init];
+  navTitleLabel.font = [ARISTemplate ARISTitleFont];
+  navTitleLabel.textAlignment = NSTextAlignmentCenter;
+  switch(viewMode)
+  {
+    case VIEW_MODE_MINE: navTitleLabel.text = NSLocalizedString(@"NotebookMyNotesKey", @"");    break;
+    case VIEW_MODE_ALL:  navTitleLabel.text = NSLocalizedString(@"NotebookAllNotesKey", @"");   break;
+    case VIEW_MODE_TAG:  navTitleLabel.text = filterTag.tag; break;
+  }
 
-    [navTitleView addSubview:navTitleLabel];
-    self.navigationItem.titleView = navTitleView;
+  [navTitleView addSubview:navTitleLabel];
+  self.navigationItem.titleView = navTitleView;
 
-    table = [[UITableView alloc] initWithFrame:self.view.frame];
-    table.contentInset = UIEdgeInsetsMake(100,0,49,0);
-    [table setContentOffset:CGPointMake(0,-100)];
-    table.delegate   = self;
-    table.dataSource = self;
+  table = [[UITableView alloc] initWithFrame:self.view.frame];
+  table.contentInset = UIEdgeInsetsMake(100,0,49,0);
+  [table setContentOffset:CGPointMake(0,-100)];
+  table.delegate   = self;
+  table.dataSource = self;
 
-    [self.view addSubview:table];
-    [self.view addSubview:searchBar];
+  [self.view addSubview:table];
+  [self.view addSubview:searchBar];
 }
 
 - (void) viewDidLayoutSubviews
 {
-    [super viewDidLayoutSubviews];
-    navTitleView.frame  = CGRectMake(self.view.bounds.size.width/2-80, 5, 160, 35);
-    navTitleLabel.frame = CGRectMake(0, 0, navTitleView.frame.size.width, navTitleView.frame.size.height);
+  [super viewDidLayoutSubviews];
+  navTitleView.frame  = CGRectMake(self.view.bounds.size.width/2-80, 5, 160, 35);
+  navTitleLabel.frame = CGRectMake(0, 0, navTitleView.frame.size.width, navTitleView.frame.size.height);
 
-    searchBar.frame = CGRectMake(-4,64,self.view.bounds.size.width+8,36);//weird width/height because apple
+  searchBar.frame = CGRectMake(-4,64,self.view.bounds.size.width+8,36);//weird width/height because apple
 
-    table.frame = self.view.frame;
+  table.frame = self.view.frame;
 }
 
 - (void) viewWillAppear:(BOOL)animated
 {
-    [super viewWillAppear:animated];
-    UIButton *backButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    backButton.frame = CGRectMake(0, 0, 19, 19);
-    [backButton setImage:[UIImage imageNamed:@"arrowBack"] forState:UIControlStateNormal];
-    [backButton addTarget:self action:@selector(backButtonTouched) forControlEvents:UIControlEventTouchUpInside];
-    backButton.accessibilityLabel = @"Back Button";
-    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:backButton];
+  [super viewWillAppear:animated];
+  UIButton *backButton = [UIButton buttonWithType:UIButtonTypeCustom];
+  backButton.frame = CGRectMake(0, 0, 19, 19);
+  [backButton setImage:[UIImage imageNamed:@"arrowBack"] forState:UIControlStateNormal];
+  [backButton addTarget:self action:@selector(backButtonTouched) forControlEvents:UIControlEventTouchUpInside];
+  backButton.accessibilityLabel = @"Back Button";
+  self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:backButton];
 
-    [table reloadData];
+  [table reloadData];
 }
 
 - (void) newNoteListAvailable
 {
-    [table reloadData];
+  [table reloadData];
 }
 
 - (void) noteDataAvailable:(NSNotification *)n
 {
-    [table reloadData];
+  [table reloadData];
 }
 
 - (NSInteger) numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 1;
+  return 1;
 }
 
 - (NSInteger) tableView:(UITableView *)t numberOfRowsInSection:(NSInteger)section
 {
-    NSArray *typeFilteredNotes;
-    if     (viewMode == VIEW_MODE_MINE) typeFilteredNotes = [_MODEL_NOTES_ playerNotes];
-    else if(viewMode == VIEW_MODE_ALL)  typeFilteredNotes = [_MODEL_NOTES_ listNotes];
-    else if(viewMode == VIEW_MODE_TAG)  typeFilteredNotes = [_MODEL_NOTES_ notesMatchingTag:filterTag];
+  NSArray *typeFilteredNotes;
+  if     (viewMode == VIEW_MODE_MINE) typeFilteredNotes = [_MODEL_NOTES_ playerNotes];
+  else if(viewMode == VIEW_MODE_ALL)  typeFilteredNotes = [_MODEL_NOTES_ listNotes];
+  else if(viewMode == VIEW_MODE_TAG)  typeFilteredNotes = [_MODEL_NOTES_ notesMatchingTag:filterTag];
 
-    NSMutableArray *textFilteredNotes;
-    if([filterText isEqualToString:@""])
-        textFilteredNotes = [NSMutableArray arrayWithArray:typeFilteredNotes];
-    else
+  NSMutableArray *textFilteredNotes;
+  if([filterText isEqualToString:@""])
+    textFilteredNotes = [NSMutableArray arrayWithArray:typeFilteredNotes];
+  else
+  {
+    Note *n;
+    textFilteredNotes = [[NSMutableArray alloc] initWithCapacity:typeFilteredNotes.count];
+    for(long i = 0; i < typeFilteredNotes.count; i++)
     {
-        Note *n;
-        textFilteredNotes = [[NSMutableArray alloc] initWithCapacity:typeFilteredNotes.count];
-        for(long i = 0; i < typeFilteredNotes.count; i++)
+      n = [typeFilteredNotes objectAtIndex:i];
+      //Search description
+      if([n.desc rangeOfString:filterText options:NSRegularExpressionSearch|NSCaseInsensitiveSearch].location != NSNotFound)
+        [textFilteredNotes addObject:n];
+      //Search owner
+      else if([[_MODEL_USERS_ userForId:n.user_id].display_name rangeOfString:filterText options:NSRegularExpressionSearch|NSCaseInsensitiveSearch].location != NSNotFound)
+        [textFilteredNotes addObject:n];
+      //Search tags
+      else
+      {
+        NSArray *tags = [_MODEL_TAGS_ tagsForObjectType:@"NOTE" id:n.note_id];
+        for(long j = 0; j < tags.count; j++)
         {
-            n = [typeFilteredNotes objectAtIndex:i];
-            //Search description
-            if([n.desc rangeOfString:filterText options:NSRegularExpressionSearch|NSCaseInsensitiveSearch].location != NSNotFound)
-                [textFilteredNotes addObject:n];
-            //Search owner
-            else if([[_MODEL_USERS_ userForId:n.user_id].display_name rangeOfString:filterText options:NSRegularExpressionSearch|NSCaseInsensitiveSearch].location != NSNotFound)
-                [textFilteredNotes addObject:n];
-            //Search tags
-            else
-            {
-                NSArray *tags = [_MODEL_TAGS_ tagsForObjectType:@"NOTE" id:n.note_id];
-                for(long j = 0; j < tags.count; j++)
-                {
-                    if([((Tag *)tags[j]).tag rangeOfString:filterText options:NSRegularExpressionSearch|NSCaseInsensitiveSearch].location != NSNotFound)
-                    {
-                        [textFilteredNotes addObject:n];
-                        break; //must break so we don't add same note multiple times
-                    }
-                }
-            }
+          if([((Tag *)tags[j]).tag rangeOfString:filterText options:NSRegularExpressionSearch|NSCaseInsensitiveSearch].location != NSNotFound)
+          {
+            [textFilteredNotes addObject:n];
+            break; //must break so we don't add same note multiple times
+          }
         }
+      }
     }
+  }
 
-    filteredNotes = _ARIS_ARRAY_SORTED_ON_(textFilteredNotes,@"created");
-    return filteredNotes.count;
+  filteredNotes = _ARIS_ARRAY_SORTED_ON_(textFilteredNotes,@"created");
+  return filteredNotes.count;
 }
 
 - (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return 78.0;
+  return 78.0;
 }
 
 - (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    NoteCell *cell;
-    if(!(cell = (NoteCell *)[table dequeueReusableCellWithIdentifier:[NoteCell cellIdentifier]]))
-        cell = [[NoteCell alloc] initWithDelegate:self];
-    Note *n = [filteredNotes objectAtIndex:indexPath.row];
-    [cell populateWithNote:n];
+  NoteCell *cell;
+  if(!(cell = (NoteCell *)[table dequeueReusableCellWithIdentifier:[NoteCell cellIdentifier]]))
+    cell = [[NoteCell alloc] initWithDelegate:self];
+  Note *n = [filteredNotes objectAtIndex:indexPath.row];
+  [cell populateWithNote:n];
 
-    return cell;
+  return cell;
 }
 
 - (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    Instance *i = [_MODEL_INSTANCES_ instanceForId:0]; //create hacked instance
-    i.object_type = @"NOTE";
-    i.object_id = ((Note *)[filteredNotes objectAtIndex:indexPath.row]).note_id;
-    i.qty = 1;
-    NoteViewController *nvc = [[NoteViewController alloc] initWithInstance:i delegate:self];
-    [self.navigationController pushViewController:nvc animated:YES];
+  Instance *i = [_MODEL_INSTANCES_ instanceForId:0]; //create hacked instance
+  i.object_type = @"NOTE";
+  i.object_id = ((Note *)[filteredNotes objectAtIndex:indexPath.row]).note_id;
+  i.qty = 1;
+  NoteViewController *nvc = [[NoteViewController alloc] initWithInstance:i delegate:self];
+  [self.navigationController pushViewController:nvc animated:YES];
 }
 
 - (void) instantiableViewControllerRequestsDismissal:(id<InstantiableViewControllerProtocol>)govc
 {
-    [self.navigationController popToViewController:self animated:YES];
+  [self.navigationController popToViewController:self animated:YES];
 }
 
 - (void) searchBar:(UISearchBar *)s textDidChange:(NSString *)t
 {
-    filterText = t;
-    searchBar.showsCancelButton = YES;
-    [table reloadData];
+  filterText = t;
+  searchBar.showsCancelButton = YES;
+  [table reloadData];
 }
 
 - (void) searchBarCancelButtonClicked:(UISearchBar *)s
 {
-    filterText = @"";
-    searchBar.text = @"";
-    [searchBar resignFirstResponder];
-    searchBar.showsCancelButton = NO;
-    [table reloadData];
+  filterText = @"";
+  searchBar.text = @"";
+  [searchBar resignFirstResponder];
+  searchBar.showsCancelButton = NO;
+  [table reloadData];
 }
 
 - (void) searchBarSearchButtonClicked:(UISearchBar *)s
 {
-    [searchBar resignFirstResponder];
-    searchBar.showsCancelButton = NO;
-    [table reloadData];
+  [searchBar resignFirstResponder];
+  searchBar.showsCancelButton = NO;
+  [table reloadData];
 }
 
 - (void) setModeAll
 {
-    viewMode = VIEW_MODE_ALL;
-    navTitleLabel.text = NSLocalizedString(@"NotebookAllNotesKey", @"");
-    [table reloadData];
+  viewMode = VIEW_MODE_ALL;
+  navTitleLabel.text = NSLocalizedString(@"NotebookAllNotesKey", @"");
+  [table reloadData];
 }
 
 - (void) setModeMine
 {
-    viewMode = VIEW_MODE_MINE;
-    navTitleLabel.text = NSLocalizedString(@"NotebookMyNotesKey", @"");
-    [table reloadData];
+  viewMode = VIEW_MODE_MINE;
+  navTitleLabel.text = NSLocalizedString(@"NotebookMyNotesKey", @"");
+  [table reloadData];
 }
 
 - (void) setModeTag:(Tag *)t
 {
-    viewMode = VIEW_MODE_TAG;
-    filterTag = t;
-    navTitleLabel.text = filterTag.tag;
-    [table reloadData];
+  viewMode = VIEW_MODE_TAG;
+  filterTag = t;
+  navTitleLabel.text = filterTag.tag;
+  [table reloadData];
 }
 
 - (void) backButtonTouched
 {
-    [delegate notesViewControllerRequestsDismissal:self];
+  [delegate notesViewControllerRequestsDismissal:self];
 }
 
 - (void) dealloc
 {
-    _ARIS_NOTIF_IGNORE_ALL_(self);
+  _ARIS_NOTIF_IGNORE_ALL_(self);
 }
 
 @end
