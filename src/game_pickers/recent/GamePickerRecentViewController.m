07b183c2618931d14ae9adf184dacd7fbefb2a2c
Phildo <Phildo211@gmail.com>
2015-04-02 12:49:35 -0500

    fixed deprecation on picker icons

--- src/game_pickers/recent/GamePickerRecentViewController.m
+++ src/game_pickers/recent/GamePickerRecentViewController.m
@@ -17,8 +17,8 @@
     if(self = [super initWithDelegate:d])
     {
         self.title = NSLocalizedString(@"GamePickerRecentTabKey", @"");
-        
-        [self.tabBarItem setFinishedSelectedImage:[UIImage imageNamed:@"clock_red.png"] withFinishedUnselectedImage:[UIImage imageNamed:@"clock.png"]];   
+        [self.tabBarItem setImage:[[UIImage imageNamed:@"clock.png"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]];
+        [self.tabBarItem setSelectedImage:[[UIImage imageNamed:@"clock_red.png"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]];
   _ARIS_NOTIF_LISTEN_(@"MODEL_RECENT_GAMES_AVAILABLE",self,@selector(recentGamesAvailable),nil);
     }
     return self;
